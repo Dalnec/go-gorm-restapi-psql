2f7120aa2932190b51022cc4c8727b8094d7b245
Dalnec <dalnec14@gmail.com>
2023-09-20 18:49:41 -0500

    init values

--- routes/brands.routes.go
+++ routes/brands.routes.go
@@ -30,3 +30,8 @@ func CreateBrandsHandler(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(&brand)
 }
+
+func initCreateBrands() {
+	brand := models.Brand{ Description: "-", Active: true } 
+	db.DB.Create(&brand)
+}
